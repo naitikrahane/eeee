ec02fd2b957285946ba2579c4f03c783a23da7d4
Lucky13y1y1 <162469602+Lucky13y1y1@users.noreply.github.com>
2024-05-13 23:27:35 +0530

    index.py

--- index.py
+++ index.py
@@ -8,8 +8,8 @@ from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryH
 from movies_scraper import search_movies, get_movie
 
 
-TOKEN = os.getenv("TOKEN")
-URL = "https://movie-bot-vert.vercel.app"
+TOKEN = "7071063008:AAFXt_BdoFpRqg29szqJQcrUZU0EUifYBvY"
+URL = "https://eeee-tau.vercel.app/"
 bot = Bot(TOKEN)
 
 
